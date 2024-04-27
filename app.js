31dbbd76d5a8b60b54c0027205042957df61ef5b
Ajay chilamsetti <ajaychilamsetti@gmail.comm>
2024-04-27 12:10:27 +0530

    add

--- app.js
+++ app.js
@@ -378,17 +378,17 @@ singinbtn.addEventListener('click', function(e) {
   });
 });
 
-// Get the logout button element
-const logoutButton = document.getElementById('logout');
-logoutButton.innerHTML = 'Logout';
-// Add click event listener to the logout button
-logoutButton.addEventListener('click', function() {
-  logoutButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" class="loadersvg"  style="fill: rgb(255, 255, 255);transform: ;msFilter:;"><circle cx="12" cy="20" r="2"></circle><circle cx="12" cy="4" r="2"></circle><circle cx="6.343" cy="17.657" r="2"></circle><circle cx="17.657" cy="6.343" r="2"></circle><circle cx="4" cy="12" r="2.001"></circle><circle cx="20" cy="12" r="2"></circle><circle cx="6.343" cy="6.344" r="2"></circle><circle cx="17.657" cy="17.658" r="2"></circle></svg>';
-  // Remove all values from localStorage
-  localStorage.clear();
-  // Refresh the page
-  location.reload();
-});
+// // Get the logout button element
+// const logoutButton = document.getElementById('logout');
+// logoutButton.innerHTML = 'Logout';
+// // Add click event listener to the logout button
+// logoutButton.addEventListener('click', function() {
+//   logoutButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" class="loadersvg"  style="fill: rgb(255, 255, 255);transform: ;msFilter:;"><circle cx="12" cy="20" r="2"></circle><circle cx="12" cy="4" r="2"></circle><circle cx="6.343" cy="17.657" r="2"></circle><circle cx="17.657" cy="6.343" r="2"></circle><circle cx="4" cy="12" r="2.001"></circle><circle cx="20" cy="12" r="2"></circle><circle cx="6.343" cy="6.344" r="2"></circle><circle cx="17.657" cy="17.658" r="2"></circle></svg>';
+//   // Remove all values from localStorage
+//   localStorage.clear();
+//   // Refresh the page
+//   location.reload();
+// });
 
 displayname.innerHTML = localStorage.getItem('storedUsername') || 'Login';
 dp.src = localStorage.getItem('storedProfileImageURL') || './Images/my2dp.jpg';
